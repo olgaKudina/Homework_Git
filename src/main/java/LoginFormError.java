f6e20b63efe96155bfb618fdc1ad25f0c265d4b7
Olga Kudina <olga.kudzina@gmail.com>
2021-04-29 17:50:02 +0300

    Initial commit

---
+++ src/main/java/LoginFormError.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginFormError {
+    @Test
+    public void LoginForm() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.name("email")).sendKeys("https://www.sharelane.com/cgi-bin/main.py");
+        driver.findElement(By.name("password")).sendKeys("1111");
+        driver.findElement(By.xpath("//*[@value='Login']")).click();
+        driver.findElement(By.className("Oops, error. Email and/or password don't match our records"));
+
+    }
+}
